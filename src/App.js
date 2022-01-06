152e5188c948c1d5f0fb22c6ff8f5a5d0a6b53bf
panuvit.chantara <panuvit.chantara@ait.asia>
2022-01-06 23:31:34 +0700

    update signin with google on firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
+// React part
+import React, { Component } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { HomePage } from './pages/homepage/HomePage';
+
+// Pages
+import HomePage from './pages/homepage/HomePage';
 import ShopPage from './components/shop/Shop'; 
+import Header from './components/header/Header';
+import { SignInAndSignUp } from './components/sign-in-and-sign-up/SignInAndSignUp';
+
+// Authentication
+import { auth } from './firebase/firebase.utils';
 
 
-function App() {
-  return (
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unSubscribeFromAuth = null
+
+  componentDidMount(){
+    this.unSubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({currentUser: user})
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return(
     <div className="App">
+    <Header currentUser={this.state.currentUser}/>
     <Routes>
       <Route exact path='/' element={<HomePage/>}/>
       <Route exact path='/shop' element={ <ShopPage/>} />
+      <Route exact path='/sign-in' element={ <SignInAndSignUp />} />
     </Routes>
     </div>
-  );
+    );
+  }
 }
+
+
 
 export default App;
